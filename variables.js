3accc5d57655010e01ea14ad1d98f5b84dd4aafd
Abubakar <167848083+AbubakarThings@users.noreply.github.com>
2024-05-14 17:54:06 +0500

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let accountEmail = "abubakarkha201@gmail.com"
+var accountPassword = "123@456"
+accountCity = "Karachi"
+
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/ 
